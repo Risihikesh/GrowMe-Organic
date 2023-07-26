a4ed19e1a8221f62b0fec8e0fece03929551a1d3
Rishikesh <krishikesh369@gmail.com>
2023-07-27 01:55:37 +0530

    done

---
+++ src/components/FirstPage.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Box, Typography } from '@mui/material';
+
+const FirstPage: React.FC = () => {
+  const history = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    phoneNumber: '',
+    email: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    localStorage.setItem('userDetails', JSON.stringify(formData));
+    history('/second-page');
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" mt={10}>
+      <Typography variant="h4">First Page</Typography>
+      <TextField
+        label="Name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        label="Phone Number"
+        name="phoneNumber"
+        value={formData.phoneNumber}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        label="Email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+      />
+      <Button variant="contained" color="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Box>
+  );
+};
+
+export default FirstPage;
